4b88acd04c75813d4dfdbfaa19099fed18b097f5
asnefedov <nefedovaleksandr196@gmail.com>
2026-02-16 17:25:11 +0500

    Небольшой рефакторинг и удаление задачи

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
   import { ref } from 'vue';
 
-  const newTodoText = ref('')
+  const newTodoText = ref<string | null>(null)
 
   const todos = ref([
     { id: 1, text: 'Изучить Vue 3', done: false },
@@ -23,7 +23,7 @@
    * Добавление задачи
    */
   function addTodo() {
-    if (!newTodoText.value.trim()) return;
+    if (!newTodoText.value || !newTodoText.value.trim()) return;
 
     const newTodo = {
       id: Date.now(),
@@ -32,6 +32,15 @@
     }
 
     todos.value.push(newTodo);
+
+    newTodoText.value = null;
+  }
+
+  /**
+   * Удаление задачи по id
+   */
+  function removeTodo(id: number) {
+    todos.value = todos.value.filter(todo => todo.id !== id);
   }
 </script>
 
@@ -45,7 +54,7 @@
   />
   <button
     @click="addTodo()"
-    v-show='newTodoText.length> 0'
+    v-show='newTodoText && newTodoText.length > 0'
   >
     Добавить
   </button>
@@ -53,6 +62,7 @@
     <li v-for="(todo, index) in todos" :key="todo.id">
       <button @click="toggleTodo(todo.id)">{{ todo.done }}</button>
       <div>{{ todo.id }} {{ index }} {{ todo.text }} </div>
+      <button @click="removeTodo(todo.id)">Удалить</button>
     </li>
   </ul>
 </template>
