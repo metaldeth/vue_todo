d27101876b50ba361f7966ded641a61a24ad36e9
asnefedov <nefedovaleksandr196@gmail.com>
2026-02-16 14:45:56 +0500

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+  const message = 'Привет, мир!'
+</script>
+
+<template>
+  <div>
+    <h1>Vue Todo</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 1.5rem;
+}
+</style>
